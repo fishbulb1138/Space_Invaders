22763cd641a803efce126366c72ac846c7132dd9
fishbulb1138 <dssanche@outlook.com>
2025-03-09 19:01:27 -0400

    added background color and frame rate timer

--- src/main.py
+++ src/main.py
@@ -8,9 +8,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize game and create resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("SPACE MONSTER HOORAY")
+        # set background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start main game loop"""
@@ -20,8 +23,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # redraw screen through each pass of the loop
+            self.screen.fill(self.bg_colorg)
+
             # make most recently drawn screen visible
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
